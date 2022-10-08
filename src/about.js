7b517011159a1a94b6ff5ae4a48f95dcd2654252
Vsein <vseinssj@gmail.com>
2022-10-08 13:11:08 +0300

    Migrate to generating HTML with JS

---
+++ src/about.js
@@ -0,0 +1,90 @@
+function createNavbar() {
+  const navbar = document.createElement('div');
+  navbar.classList.add('navbar');
+
+  const title = document.createElement('div');
+  title.classList.add('title');
+
+  const h1 = document.createElement('h1');
+  h1.textContent = 'Smoothiers';
+  title.appendChild(h1);
+  navbar.appendChild(title);
+
+  const navRight = document.createElement('div');
+  navRight.classList.add('nav-right');
+
+  var links = ['About', 'Menu', 'Contact'];
+  for (let i = 0; i < links.length; i++) {
+    const link = document.createElement('div');
+    link.classList.add('link');
+    const p = document.createElement('p');
+    p.textContent = links[i];
+    link.appendChild(p);
+    navRight.appendChild(link);
+  }
+  navbar.appendChild(navRight);
+  return navbar;
+}
+
+export default function loadAboutPage() {
+  const content = document.createElement('div');
+  content.id = 'content';
+
+  content.appendChild(createNavbar());
+
+  var sections = ['welcome', 'fresh', 'discount', 'custom'];
+  var smoothies = ['welcome', 'blueberry', 'discount', 'ginger'];
+  var texts = ["Your healthy lifestyle, as easy as it has ever been!",
+    "See how we make your smoothie and be sure that it's made only of fresh ingredients!",
+    "A special discount on all our smoothies in the morning! Jump-start your day with momentum!",
+    "Try to make a custom smoothie - you can also participate in our monthly contest of best custom smoothies! The most voted one will be added to the permanent menu."];
+  var buttons = ['', '', 'Learn more', ''];
+
+  for (let i = 0; i < sections.length; i++) {
+    let name = sections[i];
+    let imgName = smoothies[i];
+    const section = document.createElement('div');
+    section.classList.add(name);
+
+    const img = document.createElement('img');
+    img.src = `smoothie-${imgName}.jpg`;
+    img.alt = `${imgName} smoothie`;
+    img.classList.add(`${name}-smoothie`);
+
+    const container = document.createElement('div');
+    container.classList.add(`${name}-container`);
+
+    const text = document.createElement('div');
+    text.classList.add(`${name}-text`);
+
+    const p = document.createElement('div');
+    p.textContent = texts[i];
+    text.appendChild(p);
+
+    if (buttons[i]) {
+      const button = document.createElement('button');
+      button.classList.add(`${name}-button`);
+      button.textContent = buttons[i];
+      text.appendChild(button);
+    }
+
+    container.appendChild(text);
+
+    if (i % 2) {
+      section.appendChild(img);
+      section.appendChild(container);
+    } else {
+      section.appendChild(container);
+      section.appendChild(img);
+    }
+
+    content.appendChild(section);
+  }
+
+
+  const footer = document.createElement('div');
+  footer.classList.add('footer');
+  content.appendChild(footer);
+
+  document.body.appendChild(content);
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-console.log(1);
+import loadAboutPage from "./about.js";
+
+loadAboutPage();
